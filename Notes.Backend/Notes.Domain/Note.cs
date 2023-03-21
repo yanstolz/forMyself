2210e022c7ce966aa7a0b56b4ea17af2a1334576
bocharoviv <bocharoviv@bukep.ru>
2023-03-21 16:58:17 +0300

    Промежуточный комит, необходимо дработать. Работа велась над MediatR

--- Notes.Backend/Notes.Domain/Note.cs
+++ Notes.Backend/Notes.Domain/Note.cs
@@ -6,8 +6,8 @@ namespace Notes.Domain
     {
         public Guid UserId { get; set; }
         public Guid Id { get; set; }
-        public string? Title { get; set; }
-        public string? Details { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime? EditDate { get; set;}
     }
