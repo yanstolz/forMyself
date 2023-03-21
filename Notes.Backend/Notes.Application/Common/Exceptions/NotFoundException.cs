cfdc2fc5b70d45f2d57da2ce786af3c7afd05995
bocharoviv <bocharoviv@bukep.ru>
2023-03-22 00:04:13 +0300

    Добавление файлов(к прошлому коммиту)

---
+++ Notes.Backend/Notes.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Notes.Application.Common.Exception
+{
+    public class NotFoundException : IOException
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found.") { }
+    }
+}
