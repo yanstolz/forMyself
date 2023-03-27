456fe7ee640c1db9dfb666355f28178af6bc3fc9
yanstolz <sir.ivan-akotya@yandex.ru>
2023-03-27 21:39:31 +0300

    Попытался в зависимости

--- Notes.Backend/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQuery.cs
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQuery.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace Notes.Application.Notes.Queries.GetNoteDetails
 {
-    public class GetNoteDetailsQuery : IRequest<NoteDeatilsVm>
+    public class GetNoteDetailsQuery : IRequest<NoteDetailsVm>
     {
         public Guid UserId { get; set; }
         public Guid Id { get; set; }
